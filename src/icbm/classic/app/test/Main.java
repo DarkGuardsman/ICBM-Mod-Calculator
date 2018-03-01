dc8d175234690a5a8b6842a8669f3e9da59fbbcc
Robert WC Seifert <rseifert.phone@gmail.com>
2018-03-01 09:57:45 -0500

    Moved over app from ICBM-Classic

---
+++ src/icbm/classic/app/test/Main.java
@@ -0,0 +1,16 @@
+package icbm.classic.app.test;
+
+import icbm.classic.app.test.path.FramePathTest;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 2/27/2018.
+ */
+public final class Main
+{
+    public static void main(String[] args)
+    {
+        FramePathTest frame = new FramePathTest();
+        frame.setVisible(true);
+    }
+}
